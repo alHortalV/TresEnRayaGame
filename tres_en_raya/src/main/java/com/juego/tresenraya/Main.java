fc82877a58decb7d038ef7aceb1b1ff2d4b55508
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-11-21 13:50:45 +0100

    Inicialización de la clase jugador1 y jugador2 en la clase Main

--- tres_en_raya/src/main/java/com/juego/tresenraya/Main.java
+++ tres_en_raya/src/main/java/com/juego/tresenraya/Main.java
@@ -7,5 +7,11 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
+
+        Jugador jugador1 = new Jugador("X");
+        Jugador jugador2 = new Jugador("O");
+
+        jugador1.start(); // Inicia el hilo del jugador 1
+        jugador2.start(); // Inicia el hilo del jugador 2
     }
 }
