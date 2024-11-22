27e6e8555ab227318880ec3fcd1aa154ecdeddf7
edbaru <edubaldomeroruiz@gmail.com>
2024-11-22 12:45:34 +0100

    escribir las victorias en un documento de texto con los resultados

--- tres_en_raya/src/main/java/com/juego/tresenraya/Tablero.java
+++ tres_en_raya/src/main/java/com/juego/tresenraya/Tablero.java
@@ -1,5 +1,9 @@
 package com.juego.tresenraya;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 /**
  *
  * @author alejandro
@@ -13,112 +17,139 @@ public class Tablero {
     private static final int VACIO = 0; // Huecos del tablero
     private static final int JUGADOR1 = 1; // X
     private static final int JUGADOR2 = 2; // O
-
-    private AtomicReferenceArray<Integer> tablero; // Tablero de 3x3
-    private int turnoActual; // Para ver a qué jugador le toca
-    private boolean partidaTerminada; // Para comprobar si la partida ha terminado
-
-    private final ReentrantLock lock; // Bloqueo para sincronización
-    private final Condition esperaTurno; // Condición para esperar el turno
-
-    // Constructor
-    public Tablero() {
-        // Inicializamos el tablero con el valor VACIO
-        this.tablero = new AtomicReferenceArray<>(new Integer[9]); // 3x3
-        for (int i = 0; i < 9; i++) {
-            tablero.set(i, VACIO);
+    private static int victoriasJugador1 = 0;
+        private static int victoriasJugador2 = 0;
+    
+        private AtomicReferenceArray<Integer> tablero; // Tablero de 3x3
+        private int turnoActual; // Para ver a qué jugador le toca
+        private boolean partidaTerminada; // Para comprobar si la partida ha terminado
+    
+        private final ReentrantLock lock; // Bloqueo para sincronización
+        private final Condition esperaTurno; // Condición para esperar el turno
+    
+        // Constructor
+        public Tablero() {
+            // Inicializamos el tablero con el valor VACIO
+            this.tablero = new AtomicReferenceArray<>(new Integer[9]); // 3x3
+            for (int i = 0; i < 9; i++) {
+                tablero.set(i, VACIO);
+            }
+            this.turnoActual = JUGADOR1; // Empieza el jugador 1
+            this.partidaTerminada = false;
+            this.lock = new ReentrantLock(); // Inicializamos el ReentrantLock
+            this.esperaTurno = lock.newCondition(); // Creamos la condición de espera
         }
-        this.turnoActual = JUGADOR1; // Empieza el jugador 1
-        this.partidaTerminada = false;
-        this.lock = new ReentrantLock(); // Inicializamos el ReentrantLock
-        this.esperaTurno = lock.newCondition(); // Creamos la condición de espera
-    }
-
-    public boolean cambiarJugada(int fila, int columna, int jugador) {
-        lock.lock(); // Bloqueamos el acceso concurrente
-
-        try {
-            // Aseguramos que solo el jugador correspondiente haga su jugada
-            while (turnoActual != jugador) {
-                esperaTurno.await(); // Si no es su turno, el hilo se bloquea aquí
-            }
-
-            boolean jugadaValida = true;
-            int index = fila * 3 + columna; // Convertir coordenadas 2D a 1D para comprobar si la partida ha terminado
-            if (partidaTerminada || tablero.get(index) != VACIO) {
-                jugadaValida = false; // En el caso de que la partida haya terminado o haya un hueco ocupado
-            }
-
-            // Actualizamos el tablero con el turno del jugador
-            if (jugadaValida) {
-                tablero.set(index, jugador); // Usamos AtomicReferenceArray para establecer el valor
-
-                // Comprobamos si se ha terminado la partida
-                if (comprobarVictoria(jugador)) {
-                    JOptionPane.showMessageDialog(null, "Ha ganado el jugador " + jugador);
-                    partidaTerminada = true;
+    
+        public boolean cambiarJugada(int fila, int columna, int jugador) {
+            lock.lock(); // Bloqueamos el acceso concurrente
+    
+            try {
+                // Aseguramos que solo el jugador correspondiente haga su jugada
+                while (turnoActual != jugador) {
+                    esperaTurno.await(); // Si no es su turno, el hilo se bloquea aquí
                 }
-
-                if (comprobarEmpate()) {
-                    JOptionPane.showMessageDialog(null, "EMPATE");
-                    partidaTerminada = true;
+    
+                boolean jugadaValida = true;
+                int index = fila * 3 + columna; // Convertir coordenadas 2D a 1D para comprobar si la partida ha terminado
+                if (partidaTerminada || tablero.get(index) != VACIO) {
+                    jugadaValida = false; // En el caso de que la partida haya terminado o haya un hueco ocupado
                 }
-
-                // Cambiar de turno
-                turnoActual = (turnoActual == JUGADOR1) ? JUGADOR2 : JUGADOR1;
-                esperaTurno.signalAll(); // Despertamos al otro jugador (para que juegue)
+    
+                // Actualizamos el tablero con el turno del jugador
+                if (jugadaValida) {
+                    tablero.set(index, jugador); // Usamos AtomicReferenceArray para establecer el valor
+    
+                    // Comprobamos si se ha terminado la partida
+                    if (comprobarVictoria(jugador)) {
+                        JOptionPane.showMessageDialog(null, "Ha ganado el jugador " + jugador);
+                        if(jugador == 1){
+                            victoriasJugador1++;
+                        }else{
+                            victoriasJugador2++;
+                        }
+                        partidaTerminada = true;
+                    }
+    
+                    if (comprobarEmpate()) {
+                        JOptionPane.showMessageDialog(null, "EMPATE");
+                        partidaTerminada = true;
+                    }
+    
+                    // Cambiar de turno
+                    turnoActual = (turnoActual == JUGADOR1) ? JUGADOR2 : JUGADOR1;
+                    esperaTurno.signalAll(); // Despertamos al otro jugador (para que juegue)
+                }
+    
+                return jugadaValida; // Devolvemos si la jugada ha sido válida o no
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Restaurar el estado de la interrupción
+                return false;
+            } finally {
+                lock.unlock(); // Desbloqueamos para que otro hilo pueda acceder
             }
-
-            return jugadaValida; // Devolvemos si la jugada ha sido válida o no
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt(); // Restaurar el estado de la interrupción
-            return false;
-        } finally {
-            lock.unlock(); // Desbloqueamos para que otro hilo pueda acceder
         }
-    }
-
-    public boolean comprobarVictoria(int jugador) {
-        boolean victoria = false;
-        // Se hará un bucle para comprobar las posibles formas de ganar
-        for (int i = 0; i < 3; i++) {
-            // Filas
-            if (tablero.get(i * 3) == jugador && tablero.get(i * 3 + 1) == jugador
-                    && tablero.get(i * 3 + 2) == jugador) {
+    
+        public boolean comprobarVictoria(int jugador) {
+            boolean victoria = false;
+            // Se hará un bucle para comprobar las posibles formas de ganar
+            for (int i = 0; i < 3; i++) {
+                // Filas
+                if (tablero.get(i * 3) == jugador && tablero.get(i * 3 + 1) == jugador
+                        && tablero.get(i * 3 + 2) == jugador) {
+                    victoria = true;
+                }
+                // Columnas
+                if (tablero.get(0 + i) == jugador && tablero.get(3 + i) == jugador && tablero.get(6 + i) == jugador) {
+                    victoria = true;
+                }
+            }
+    
+            // Diagonales
+            if (tablero.get(0) == jugador && tablero.get(4) == jugador && tablero.get(8) == jugador) {
                 victoria = true;
             }
-            // Columnas
-            if (tablero.get(0 + i) == jugador && tablero.get(3 + i) == jugador && tablero.get(6 + i) == jugador) {
+            if (tablero.get(2) == jugador && tablero.get(4) == jugador && tablero.get(6) == jugador) {
                 victoria = true;
             }
+    
+            return victoria;
         }
-
-        // Diagonales
-        if (tablero.get(0) == jugador && tablero.get(4) == jugador && tablero.get(8) == jugador) {
-            victoria = true;
-        }
-        if (tablero.get(2) == jugador && tablero.get(4) == jugador && tablero.get(6) == jugador) {
-            victoria = true;
-        }
-
-        return victoria;
-    }
-
-    public boolean comprobarEmpate() {
-        boolean empate = true;
-
-        // Bucle creado para recorrer el tablero
-        // Se comprobará que todos los huecos están ocupados y que no se ha ganado
-        for (int i = 0; i < 9; i++) {
-            if (tablero.get(i) == VACIO) {
-                empate = false; // Si hay al menos un hueco vacío, no hay empate
-                break; // No es necesario seguir buscando
+    
+        public boolean comprobarEmpate() {
+            boolean empate = true;
+    
+            // Bucle creado para recorrer el tablero
+            // Se comprobará que todos los huecos están ocupados y que no se ha ganado
+            for (int i = 0; i < 9; i++) {
+                if (tablero.get(i) == VACIO) {
+                    empate = false; // Si hay al menos un hueco vacío, no hay empate
+                    break; // No es necesario seguir buscando
+                }
             }
+            return empate && !partidaTerminada; // Debe ser empate y la partida no debe haber terminado
         }
-        return empate && !partidaTerminada; // Debe ser empate y la partida no debe haber terminado
-    }
-
-    public boolean isPartidaTerminada() {
-        return partidaTerminada;
+    
+        public boolean isPartidaTerminada() {
+            return partidaTerminada;
+        }
+    
+        public static void escribirVictoriaDocumentoTexto(int jugador){
+            try{
+                LocalDateTime fechaHora = LocalDateTime.now();
+                File archivo = new File("Victorias.txt");
+                FileWriter escribir = new FileWriter(archivo, true);
+                escribir.write(fechaHora.toString()+ "\n");
+                escribir.write("Victoria del jugador " + jugador + "\n");
+                if(jugador == 1){
+                    victoriasJugador1++;
+                }else{
+                    victoriasJugador2++;
+                }
+                escribir.write("VictoriasJugador1: " + victoriasJugador1 + "\n");
+                escribir.write("VictoriasJugador2: " + victoriasJugador2 + "\n");
+                escribir.write("------------------------\n");
+                escribir.close();
+            }catch (IOException e) {
+        }
     }
 }
